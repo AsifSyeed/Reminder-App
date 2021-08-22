b2d8f24987719fb295a7525e39e83255b057ec8e
AsifSyeed <asifsyeed666@gmail.com>
2021-08-22 16:42:36 +0600

    initial

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by BS-236 on 22/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
